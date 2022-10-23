c8dc3276668ec4207e26eb6005e06129bfae52ba
alexey-ryabkov <xelaris@ya.ru>
2022-10-23 21:55:06 +0300

    change page rooting

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const DEFAULT_PORT = 3000;
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
-
-// fs.readdirSync(`${__dirname}/src/pages/`).forEach(page => app.use(`/${page}`, express.static(`${__dirname}/dist`)));
-
 app.use('/', function(req, res) 
 {
     res.redirect(DEFAULT_PAGE);

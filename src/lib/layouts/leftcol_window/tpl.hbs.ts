44f773b775068dd4189cd6301823d6fea53654d1
alexey-ryabkov <xelaris@ya.ru>
2022-11-05 15:06:59 +0300

    leftcol win layout completion

--- src/lib/layouts/leftcol_window/tpl.hbs.ts
+++ src/lib/layouts/leftcol_window/tpl.hbs.ts
@@ -1,19 +1,18 @@
 export default `
-<div class="icontainer icontainer--bg_grayLight icontainer--dropshadow icontainer--size_big _leftcolWindowLayout__content">
-    <div class="icontainer__header">
-        <h1 class="caption _leftcolWindowLayout__caption">
-            <span class="caption__headline caption__headline--size_h1">Чаты</span>
-        </h1>
+<div class="_leftcolWindowLayout__wrapper">
+    <div class="icontainer icontainer--bg_glass icontainer--dropshadow icontainer--size_big _leftcolWindowLayout__container">
+        <div class="icontainer__header icontainer__header--bg_grayLight _leftcolWindowLayout__conrainerHeader"></div>
+        {{#with areas}} 
+        <div class="icontainer__content _leftcolWindowLayout__content">
+            <section class="_leftcolWindowLayout__leftcol">
+                {{{leftcol}}}        
+            </section>
+            <section class="_leftcolWindowLayout__workarea">
+                {{{workarea}}}        
+            </div>
+        </section>
+        {{/with}}
+        <div class="icontainer__footer icontainer__footer--bg_grayLight _leftcolWindowLayout__conrainerFooter"></div>
     </div>
-    {{#with areas}} 
-    <div class="icontainer__content" style="display: flex; flex-direction: row;">
-        <div style="width: 30%">
-            {{{leftcol}}}        
-        </div>
-        <div style="width: 70%">
-            {{{workarea}}}        
-        </div>
-    </div>
-    {{/with}}
 </div>
 `.trim();

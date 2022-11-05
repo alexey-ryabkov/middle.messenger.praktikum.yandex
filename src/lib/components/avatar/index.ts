d329473484c963f2c716dd1715994e73d233b2f0
alexey-ryabkov <xelaris@ya.ru>
2022-11-05 07:23:24 +0300

    add avatar component

---
+++ src/lib/components/avatar/index.ts
@@ -0,0 +1,32 @@
+import Templator from '@models/templator';
+import ComponentBlock from '@models/component_block';
+import './style.scss';
+import {BemParams} from '@models/bem_block';
+import {BlockProps} from '@models/block';
+import tpl from './tpl.hbs';
+
+
+const template = new Templator(tpl);
+
+export type AvatarProps = BlockProps & 
+{
+    img : string,
+    size? : 'small' | 'regular'
+};
+export default class Avatar extends ComponentBlock 
+{
+    constructor (props : AvatarProps)
+    {
+        const bem : BemParams = { name: 'avatar', mods: {block: []} };
+
+        if ('size' in props && bem?.mods?.block)
+        {
+            bem.mods.block.push([ 'size', props.size ]);
+        }
+        super({ props, bem });
+    }
+    protected get _template () 
+    {
+        return template;
+    }
+}

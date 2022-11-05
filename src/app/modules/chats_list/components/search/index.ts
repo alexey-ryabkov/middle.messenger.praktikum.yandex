5a2bbb39c08e81a0b900a32687eeea48852d18e7
alexey-ryabkov <xelaris@ya.ru>
2022-11-05 14:18:29 +0300

    add search component

--- src/app/modules/chats_list/components/search/index.ts
+++ src/app/modules/chats_list/components/search/index.ts
@@ -1,9 +1,11 @@
 import Templator from '@models/templator';
 import ComponentBlock from '@models/component_block';
-import {BlockProps} from '@models/block';
+import {BlockProps, BlockEvents} from '@models/block';
+import {BemParams} from '@models/bem_block';
 import InputText from '@lib-components/input-text';
 import Icon, {IconVar} from '@lib-components/icon';
 import tpl from './tpl.hbs';
+
 
 const template = new Templator(tpl);
 
@@ -13,19 +15,22 @@ export type SearchProps = BlockProps &
 };
 export default class SearchComponent extends ComponentBlock 
 {
-    constructor (props : SearchProps)
+    constructor (props : SearchProps, events? : BlockEvents)
     {
-        const icon = new Icon({ variant: IconVar.search });
+        const icon = new Icon({ variant: IconVar.search });        
+        icon.bemMix([ 'inputText', 'icon' ]);
         
         const input = new InputText({ 
             name: props.inputName, 
             plaseholder: 'Поиск по чатам' 
 
-        }, ['keyup', () => console.log('type in search phrase')]);
-        
-        icon.bemMix([ 'inputText', 'icon' ]);
+        }, events);
 
-        super({ props: {icon, input}, bem: {name: 'search'} });
+        const bem = { name: 'search', mix: { block: [[ 'inputText', [['iconed']] ]] }} as BemParams;
+
+        super({ node: input.element, props: {icon, input}, bem });
+
+        this.mount();
     }
     protected get _template () 
     {

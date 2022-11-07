928cc652af546bf2517a2fc30cfae2a3ba59d094
alexey-ryabkov <xelaris@ya.ru>
2022-11-07 07:43:22 +0300

    http-transport completion

--- src/lib/models/http_transport.ts
+++ src/lib/models/http_transport.ts
@@ -1,56 +1,54 @@
 import {isJsonString} from '../utils'
 
-export function queryStringify (data = {}) 
-{
-return Object.entries(data).map(([key, value]) => `${key}=${value}`).join('&');
-}
-
 export function httpQueryStringify (data : Record< string, any >) 
 {
-    const keys = Object.keys(data);
-    
-    return keys.reduce((result, key, index) => 
-    {
-        return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
-    }, '?');
+    return Object.entries(data).map( ([key, value]) => `${key}=${value}` ).join('&');
 }
+
 export enum Methods {
     GET = 'GET',
     PUT = 'PUT',
     POST = 'POST',
     DELETE = 'DELETE'
 }
-type HttpTransportOptions = 
+export type HttpTranspOpts = 
 {
-    method? : Methods,
     data? : any,
     headers? : Record< string, string >,
     timeout? : number
 }
+export type HttpTranspOptsFull = HttpTranspOpts &
+{
+    method? : Methods
+}
+
 export default class HttpTransport
 {
-    get = (url : string, options : HttpTransportOptions = {}) => this.request(url, {...options, method: Methods.GET}, options.timeout);
+    get = (url : string, options : HttpTranspOpts = {}) => this.request( url, {...options, method: Methods.GET} );    
+    put = (url : string, options : HttpTranspOpts = {}) => this.request(url, {...options, method: Methods.PUT});
+    post = (url : string, options : HttpTranspOpts = {}) => this.request(url, {...options, method: Methods.POST});
+    delete = (url : string, options : HttpTranspOpts = {}) => this.request(url, {...options, method: Methods.DELETE});
 
-    put = (url : string, options : HttpTransportOptions = {}) => this.request(url, {...options, method: Methods.PUT}, options.timeout);
-
-    post = (url : string, options : HttpTransportOptions = {}) => this.request(url, {...options, method: Methods.POST}, options.timeout);
-
-    delete = (url : string, options : HttpTransportOptions = {}) => this.request(url, {...options, method: Methods.DELETE}, options.timeout);
-
-    request = (url = '/', options : HttpTransportOptions = {}, timeout = 5000) => 
+    request = (url : string, options : HttpTranspOptsFull = {}) => 
     {
-        const {method = Methods.GET, data = {}, headers = {}} = options;
+        const {method = Methods.GET, data, headers = {}, timeout = 5000} = options;
+        const isGetMethod = Methods.GET == method;
+
+        if (isGetMethod && typeof data == 'object') 
+        {
+            const stringifiedQuery = httpQueryStringify( data as Record< string, any > );
+
+            if (stringifiedQuery)
+            {
+                url += ( url.includes('?') ? '&' : '?' ) + stringifiedQuery;
+            }
+        }
 
         return new Promise((resolve, reject) => 
         {
             const xhr = new XMLHttpRequest();
 
-            if (Methods.GET == method && typeof data == 'object') 
-            {
-                url += '?'+queryStringify(data as Record< string, any >);
-            }
             xhr.open(method, url);
-
             xhr.timeout = timeout;
 
             xhr.onload = () => resolve(xhr);
@@ -62,12 +60,12 @@ export default class HttpTransport
             {
                 Object.entries(headers).forEach(([name, value]) => xhr.setRequestHeader(name, value));
             }
-            if ('string' == typeof data && isJsonString(data)) 
+            if (typeof data == 'string' && data.length && isJsonString(data)) 
             {
                 xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
             }
 
-            xhr.send(data);
+            isGetMethod ? xhr.send() : xhr.send(data);
         });      
     };
 }

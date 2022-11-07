393a12a3f484baa6de7f28e60f1da760e7faa6cc
alexey-ryabkov <xelaris@ya.ru>
2022-11-07 08:08:16 +0300

    clearing off, del comments, refactor a little

--- src/app/modules/messages/tpl.hbs.ts
+++ src/app/modules/messages/tpl.hbs.ts
@@ -1,19 +1,17 @@
 export default `
-<div class="_messages">
-    <ul class="_messages__box">
-        <li class="_messages__dayGroup">
-            <div class="_messages__day">{{messagesDay}}</div>    
-            <ul class="_messages__list">
-                {{#each messages}}
-                    {{{this}}}
-                {{/each}}
-            </ul>
-        </li>
-    </ul>
-    <div class="_messages__sendMessage _sendMessage">
-        {{{inputSend}}}
-        {{{buttonSend}}}
-        {{{buttonAttach}}}
-    </div>    
-</div>
+<ul class="_messages__box">
+    <li class="_messages__dayGroup">
+        <div class="_messages__day">{{messagesDay}}</div>    
+        <ul class="_messages__list">
+            {{#each messages}}
+                {{{this}}}
+            {{/each}}
+        </ul>
+    </li>
+</ul>
+<div class="_messages__sendMessage _sendMessage">
+    {{{inputSend}}}
+    {{{buttonSend}}}
+    {{{buttonAttach}}}
+</div>    
 `.trim();

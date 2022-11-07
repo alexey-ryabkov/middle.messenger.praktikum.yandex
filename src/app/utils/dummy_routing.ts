90da2fb8a966403c7aeb0bb96cc1afa23eb34e6b
alexey-ryabkov <xelaris@ya.ru>
2022-11-07 04:53:35 +0300

    alloc dummy page routing in separ module

---
+++ src/app/utils/dummy_routing.ts
@@ -0,0 +1,20 @@
+import SurChat from '@app';
+import Page from '@models/page';
+import pages from '../pages';
+
+const defaultPage = pages['auth'];
+const fallbackPage = pages['error404'];
+
+export const getPage = (pageName : string, defPage : Page = defaultPage) : Page => pageName in pages ? pages[pageName] : defPage; 
+
+export const renderPage = (page : Page) => { SurChat.instance.page = page };
+
+export const pageNameFromUrl = (url = location.href) => Page.nameFromUrl(url);
+
+export const renderPageWithName = (pageName : string) => renderPage( getPage(pageName) );
+
+export const renderCurPage = () => renderPageWithName( pageNameFromUrl() );
+
+const go2page = (href : string) => renderPage( getPage( pageNameFromUrl(href), fallbackPage ));
+
+export default go2page;
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,19 @@
-//require('module-alias/register');
-import SurChat from './app';
-import Page from '@models/page';
-import pages from './app/pages';
+import go2page, {renderCurPage} from '@app-utils/dummy_routing';
 
-const app = SurChat.instance;
-
-// dummy page routing
-
-const url = location.href;
-const getPage = (pageName : string, defPage : Page = pages['chats']) : Page => pageName in pages ? pages[pageName] : defPage; 
-// const go2page = (pageName : string) => location.replace(Page.url(pageName));
-const renderPage = (page : Page) => { app.page = page };
-
-const pageName = Page.nameFromUrl(url);
-
-// if (app.user && 'chats' != pageName)
-// {
-//     go2page('chats');
-// }
-// else if (!app.user && 'chats' == pageName)
-// {
-//     go2page('auth');
-// }
-renderPage(getPage(pageName));
+renderCurPage();
 
 document.body.addEventListener('click', event =>
 {
     const element = event.target as Element;
 
     if ('A' == element.tagName) 
-    // TODO ссылкам, которые могут вызывать рутинг надо специальный атрибут добавить 
     {
         event.preventDefault();
+
         const href = element.getAttribute('href');
         if (href)
         {
-            renderPage( getPage( Page.nameFromUrl(href), pages['error404'] ) );
+            go2page(href);
         }        
     }
 });

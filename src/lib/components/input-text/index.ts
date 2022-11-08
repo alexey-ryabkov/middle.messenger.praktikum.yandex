9a0112cb58a408118dfd11c27f11ba1a0a716b52
alexey-ryabkov <xelaris@ya.ru>
2022-11-08 10:13:58 +0300

    rework input-text for new form types

--- src/lib/components/input-text/index.ts
+++ src/lib/components/input-text/index.ts
@@ -1,22 +1,45 @@
 import Templator from '@models/templator';
-import ComponentBlock from '@models/component_block';
-import {BlockProps, BlockEvents} from '@models/block';
-import {InputTextField} from '@models/types';
+import {BlockEvents} from '@models/block';
+import FormFieldComponent, {FormFieldProps} from '@models/form_field';
 import tpl from './tpl.hbs';
 import './style.scss';
 
-export type InputTextProps = BlockProps & InputTextField;
-export default class InputText extends ComponentBlock 
+export type InputTextProps = FormFieldProps & 
+{
+    type? : 'text' | 'password',
+    placeholder? : string,
+    autocomplete? : 'on' | 'off'
+};
+
+export default class InputText extends FormFieldComponent
 {
     constructor (props : InputTextProps, events : BlockEvents = [])
     {
         const {name, type = 'text', placeholder = '', autocomplete = 'off'} = props;
-
+        
         super({ bem: {
             name: 'inputText', 
             attrs: { elems: { 'input': {name, type, placeholder, autocomplete} }}, 
             events: { elems: { 'input': events }} 
         }});
+
+        if (props.value)
+        {
+            this.value = props.value;
+        }
+    }
+    get value () 
+    {
+        return this._input.value;
+    }
+    set value (value : string) 
+    {
+        this._input.value = value;
+    }
+    protected get _input ()
+    {
+        const input = <unknown> this.elems['input'];
+        return (input as HTMLInputElement);
     }
     protected get _template () 
     {
--- src/lib/components/input-text/tpl.hbs.ts
+++ src/lib/components/input-text/tpl.hbs.ts
@@ -1,3 +1,3 @@
 export default `
-<input class="inputText__input" bem-element="input" />
+<input class="inputText__input" />
 `.trim();

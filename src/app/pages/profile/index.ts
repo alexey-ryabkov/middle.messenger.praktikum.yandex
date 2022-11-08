56a2738adafca888c7f4446b311b589cc4d39841
alexey-ryabkov <xelaris@ya.ru>
2022-11-08 15:27:43 +0300

    completion profile page

--- src/app/pages/profile/index.ts
+++ src/app/pages/profile/index.ts
@@ -3,62 +3,90 @@ import Page from '@models/page';
 import Form from '@lib-modules/form';
 import CenteredFormLayout from '@lib-layouts/centered_form';
 import {isEmptyValidator, lengthValidator, phoneValidator} from '@lib-utils/form_validation';
+import InputText from '@lib-components/input-text';
+import InputImage from '@lib-components/input-image';
 import go2page from '@app-utils/dummy_routing';
 
+const app = SurChat.instance;
+const user = app.user;
+
+let form : Form | '' = '';
+
+if (user)
+{
+    const userProfile = user.profile;
+
+    form = new Form(
+    {
+        formFields: [                
+        [
+            new InputImage({
+                name: 'avatar',
+                label: 'Аватар',
+                image: userProfile.image
+            })
+        ], [
+            new InputText({
+                name: 'nickname',
+                label: 'Никнейм',   
+                value: userProfile.nickname                 
+            }),
+            [
+                [ ['focus', 'blur'], isEmptyValidator ], 
+                [ ['focus', 'blur'], lengthValidator, [3, 20] ]
+            ]
+        ], [
+            new InputText({
+                name: 'phone',
+                label: 'Телефон',   
+                value: userProfile.phone                 
+            }),
+            [
+                [ ['focus', 'blur'], isEmptyValidator ], 
+                [ ['focus', 'blur', 'keyup'], phoneValidator ]
+            ]
+        ], [
+            new InputText({
+                name: 'email',
+                label: 'Почта',   
+                value: userProfile.email                 
+            }),
+            [
+                [ ['focus', 'blur'], isEmptyValidator ]
+            ]
+        ], [
+            new InputText({
+                name: 'first_name',
+                label: 'Имя',   
+                value: userProfile.first_name                 
+            })
+        ], [
+            new InputText({
+                name: 'last_name',
+                label: 'Фамилия',   
+                value: userProfile.last_name                 
+            })
+        ]],
+        btnLabel: 'Сохранить',
+        onSuccess: () => go2page( Page.url('chats') ),
+        link: {
+            url: Page.url('chats'),
+            title: 'к чатам'
+        }
+    });
+    form.bemMix(['_centeredFormLayout', 'form']);
+}
+
 const blockName = '_pageProfile';
-const pageName = 'Профиль';
-const layout = new CenteredFormLayout(SurChat.instance, {title: pageName});
+const pageName = 'Редактирование профиля';
+const layout = new CenteredFormLayout(app, {title: pageName});
 
 const page = new class extends Page
 {
     protected _processPageLayout ()
     {
-        super._processPageLayout(); 
-
-        const form = new Form(
-        {
-
-            // TODO нужно передавать компоненты InputText и проч. 
-            // TODO также тут нужны текущие данные в value 
-            // TODO и видимо страница доступа
-
-            formFields: [{
-                    name: 'avatar',
-                    label: 'Аватар',
-                    type: 'image'
-                }, {
-                    name: 'nickname',
-                    label: 'Никнейм',
-                    validatorDefs: [
-                        [isEmptyValidator],
-                        [lengthValidator, [3, 20]]
-                ]}, {
-                    name: 'phone',
-                    label: 'Телефон',
-                    validatorDefs: [
-                        [isEmptyValidator],
-                        [phoneValidator]]
-                }, {
-                    name: 'email',
-                    label: 'Почта',
-                    validatorDefs: [[isEmptyValidator]]
-                }, {
-                    name: 'first_name',
-                    label: 'Имя'
-                }, {
-                    name: 'second_name',
-                    label: 'Фамилия'
-                }],
-            btnLabel: 'Сохранить',
-            onSuccess: () => go2page( Page.url('chats') ),
-            link: {
-                url: Page.url('chats'),
-                title: 'к чатам'
-            }
-        });
-
-        form.bemMix(['_centeredFormLayout', 'form']);
-
+        super._processPageLayout();
+        
         this._layout.areas = {form};
         this._layout.elemBemMix('content', [blockName, 'content']); 
     }

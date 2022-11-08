faf4fb1e99e9f568f649d72bc57c6eb120ddab9c
alexey-ryabkov <xelaris@ya.ru>
2022-11-08 11:53:32 +0300

    add input image comp

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -39,3 +39,8 @@ export function toArr<T> (item: Iterable<T>) : T[]
 {
     return [...item]; 
 }
+export function freezeEvent (event : Event) 
+{
+    event.preventDefault();
+    event.stopPropagation();
+}

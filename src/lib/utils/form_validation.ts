8e98f3b85dc942470b3b267190601b53a131497c
alexey-ryabkov <xelaris@ya.ru>
2022-11-09 07:16:59 +0300

    setup validators as at task (ohw-ah there`s task about)

--- src/lib/utils/form_validation.ts
+++ src/lib/utils/form_validation.ts
@@ -6,6 +6,15 @@ export type FieldValidatorDef = [string[], FieldValidator, object?]; // [events,
 
 export type FormFieldDef = [FormField, FieldValidatorDef[]?]; 
 
+export function validateField (field : FormField, validatorDefs : FieldValidatorDef[], errorStack : string[] = [])
+{
+    return validatorDefs.every(validatorDef => 
+    {
+        const [, validator, params] = validatorDef;
+        
+        return validator(field, errorStack, params);
+     });
+}
 export function isEmptyValidator (field : FormField, errorStack : string[] = []) : boolean
 {
     const isValid = !!field.value?.trim();
@@ -27,23 +36,38 @@ export function lengthValidator (field : FormField, errorStack : string[] = [], 
     }
     return isValid;
 }
-export function phoneValidator (field : FormField, errorStack : string[] = []) : boolean
+export function regexpValidator (field : FormField, errorStack : string[] = [], params : { regexp: RegExp, error?: string }) : boolean 
 {
     const value = field.value?.trim();
-    const isValid = !!value && /^\+?[0-9\s)(-]+$/.test(value);
+    const {
+        regexp, 
+        error = `Поле "${field.label}" заполнено некорректно`
+    } = params;
 
+    const isValid = !!value && regexp.test(value);
     if (!isValid)
     {
-        errorStack.push(`Длина значения для поля "${field.label}" некорректна, диапазон ${length}`);
+        errorStack.push(error); 
     }
     return isValid;
 }
-export function validateField (field : FormField, validatorDefs : FieldValidatorDef[], errorStack : string[] = [])
+export function nameValidator (field : FormField, errorStack : string[] = []) 
 {
-    return validatorDefs.every(validatorDef => 
-    {
-        const [, validator, params] = validatorDef;
-        
-        return validator(field, errorStack, params);
-     });
+    return regexpValidator(field, errorStack, { regexp: /^[A-ZА-Я][a-zA-Zа-яА-Я-]+$/ });
+}
+export function loginValidator (field : FormField, errorStack : string[] = []) 
+{
+    return regexpValidator(field, errorStack, { regexp: /^[a-z_-]*[0-9]?[a-z_-]+$/i });
+}
+export function phoneValidator (field : FormField, errorStack : string[] = []) 
+{
+    return regexpValidator(field, errorStack, { regexp: /^\+?[0-9]+$/ });
+}
+export function emailValidator (field : FormField, errorStack : string[] = [])
+{
+    return regexpValidator(field, errorStack, { regexp: /^[\w-]+@[\w-]+\.[a-z]+$/i });
+}
+export function passwordValidator (field : FormField, errorStack : string[] = [])
+{
+    return regexpValidator(field, errorStack, { regexp: /(?:[0-9].*[A-Z])|(?:[A-Z].*[0-9])/ });
 }

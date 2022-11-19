7a1e2882518cbc67512bf248d8ddf0b50654917f
alexey-ryabkov <xelaris@ya.ru>
2022-11-20 01:47:17 +0300

    improve event-bus

--- src/lib/models/event_bus.ts
+++ src/lib/models/event_bus.ts
@@ -2,34 +2,57 @@ import {Handler} from '@models/types';
 
 export default class EventBus
 {
-    listeners : Record< string, Handler[] >;
+    protected _listeners : Record< string, Handler[] >;
+    protected _oneTimes : Record< string, Handler[] >;
 
     constructor() 
     {
-        this.listeners = {};
+        this._listeners = {};
+        this._oneTimes = {};
     }
     on (event : string, callback : Handler) 
     {
-        if (!(event in this.listeners))
+        if (!(event in this._listeners))
         {
-            this.listeners[event] = [];
+            this._listeners[event] = [];
         }
-        this.listeners[event].push(callback);
+        this._listeners[event].push(callback);
+        return this;
     }
     off (event : string, callback : Handler) 
     {
-        if (event in this.listeners) 
+        if (event in this._listeners) 
         {
-            this.listeners[event] = this.listeners[event].filter(listener => listener !== callback);
+            this._listeners[event] = this._listeners[event].filter(listener => listener !== callback);
         }
+        if (event in this._oneTimes) 
+        {
+            this._oneTimes[event] = this._oneTimes[event].filter(listener => listener !== callback);
+        }
+        return this;
+    }
+    oneTime (event : string, callback : Handler)
+    {
+        if (!(event in this._oneTimes))
+        {
+            this._oneTimes[event] = [];
+        }
+        this._oneTimes[event].push(callback);
+        return this;
     }
     emit (event : string, ...args: any[]) 
     {
-        if (event in this.listeners) 
+        if (event in this._listeners) 
         {
-            this.listeners[event].forEach(listener => listener(...args));
+            this._listeners[event].forEach(listener => listener(...args));
         }
-        else
-            throw `Error: no event with name ${event}`;
+        if (event in this._oneTimes) 
+        {
+            this._oneTimes[event].forEach(listener => 
+            {
+                listener(...args);
+            });
+            delete this._oneTimes[event];
+        }
     }
 }

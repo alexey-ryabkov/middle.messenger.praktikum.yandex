183f9506d6bf57c6f7b7364ecb0fdd6fa052ca64
alexey-ryabkov <xelaris@ya.ru>
2022-11-26 09:41:31 +0300

    rework component block

--- src/lib/core/block/component.ts
+++ src/lib/core/block/component.ts
@@ -1,11 +1,34 @@
 import {CompilableTemplate} from '@core/types';
-import BemBlock from '@core/block/bem';
+import {BlockEvents, BlockProps} from '@core/block';
+import BemBlock, {BemCompParams, BemParams} from '@core/block/bem';
 
-export default abstract class ComponentBlock extends BemBlock 
+export type ComponentParams = Omit< BemCompParams, 'props' | 'events' | 'bem' >;
+
+export default class ComponentBlock extends BemBlock 
 {
+    constructor (
+        props? : BlockProps, 
+        events : BlockEvents = [], 
+        params : ComponentParams = {}) 
+    {   
+        // temporary stub for bem, that will be defined by _prepareBemParams method 
+        super({props, events, bem: {name: 'block'}, ...params});
+    }
+    protected _processParams (params : BemCompParams)
+    {
+        super._processParams({ ...params, bem: this._prepareBemParams(params) });
+    }    
     render ()
     {
         return this.compile(this._template); 
     }
-    protected abstract get _template () : CompilableTemplate; 
+    // can`t define method and class as an abstract cause of @core/state/connect funcs    
+    protected _prepareBemParams (params? : BemCompParams) : BemParams
+    {
+        throw new Error('ComponentBlock _prepareBemCompParams method must be overridden in the subclass');
+    }
+    protected get _template () : CompilableTemplate
+    {
+        throw new Error('ComponentBlock _template getter should be overridden in the subclass');
+    }    
 }

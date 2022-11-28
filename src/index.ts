c21796204ee55b133b6bba23c3a568f758110cf2
alexey-ryabkov <xelaris@ya.ru>
2022-11-28 12:35:31 +0300

    change app entry point to work with action

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+
 import SurChat from '@app';
 import * as pages from '@pages';
+import Actions from '@actions';
 
 const app = SurChat.instance;
-
 app.pages = Object.values( pages );
-app.init();
+
+Actions.defineUser()
+    .finally(() => 
+    {
+        app.init(); 
+    });

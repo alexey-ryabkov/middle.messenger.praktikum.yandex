0d5c45e9657973a4e99f93ef345b39e09e228ceb
alexey-ryabkov <xelaris@ya.ru>
2022-11-28 12:34:00 +0300

    make chat model

--- src/app/models/chat.ts
+++ src/app/models/chat.ts
@@ -1,44 +1,150 @@
-import MessagesApi, {MsgApi, MsgApiEvents} from '@api/messages';
-import User from '@models/user';
-import chatProfile from '@data/buddy.json'; 
-import chatMessages from '@data/messages.json'; 
+import SurChat from '@app';
+import {ChatFields, ChatMessage, Message, MessageType} from '@models/types';
+import {apiErrorHandler} from '@api/rest';
+import chatsApi from '@api/chats';
+import Messenger, {MessengerEvents} from '@api/messages';
+import CurrentUser from '@models/current_user';
+import ChatUser from '@models/chat_user';
+import Actions from '@actions';
 
-// импортируем chatsApi
-
-export default class Chat 
+export enum ChatType
 {
-    // protected _msgApi : MsgApi | null = null;
-    // constructor (chatId : number, user : User)
-    // {
-    //     chatsApi.getChatToken(chatId)
-    //                 .then(token => 
-    //                 {
-    //                     this._msgApi = new MessagesApi(user.id, chatId, token);
+    user = 'user',
+    group = 'group',
+}
+export default class Chat
+{
+    unreadCnt : number;
 
-    //                     this._msgApi.on(MsgApiEvents.error, error => console.error(error));
+    protected _id : number;
+    protected _createdBy : number;
+    protected _avatar : string | null;
+    protected _token : string;
+    protected _title : string;
+    protected _members: number[];
+    protected _messenger : Messenger;
+    protected _userId : number;
+    protected _type : ChatType;
 
-    //                     // this._msgApi.on(MsgApiEvents.message, () => {});
-    //                     // this._msgApi.on(MsgApiEvents.hist, () => {});
-    //                 });
-    // }
-    get profile ()
+    constructor (
+        protected _app : SurChat, 
+        fields : ChatFields)
     {
-        // parcel can ref to image, only if it static string
-        // so we can`t do it dynamically (get image from json)  
-        const imageUrl = new URL(
-            '../../../static/images/hatt.jpg',
-            import.meta.url
-        );
-        chatProfile.image = imageUrl.pathname;
-        
-        return chatProfile;
+        ({
+            id: this._id,
+            createdBy: this._createdBy = 0,
+            avatar: this._avatar = null,            
+            title: this._title = '',
+            unreadCnt: this.unreadCnt = 0,
+            members: this._members = [],             
+        } = fields);
+
+        this._userId = (this._app.user as CurrentUser).id;
+
+        (this._messenger = new Messenger( this._userId, this._id, this._token ))
+                .on(MessengerEvents.message, message => 
+                {
+                    Actions.recieveMessage(this._id, message as Message);
+                })
+                .on(MessengerEvents.error, error => console.error(error));
+
+        this._processChatType();
     }
+    get id ()
+    {
+        return this._id;
+    }
+    get title ()
+    {
+        if (ChatType.user == this._type)
+        {
+            const collocutor = this.collocutor;
+
+            if (collocutor)
+            {
+                return collocutor.nickname;
+            }
+        }
+        return this._title;        
+    }
+    get avatar ()
+    {
+        if (ChatType.user == this._type)
+        {
+            const collocutor = this.collocutor;
+
+            if (collocutor)
+            {
+                return collocutor.avatar;
+            }
+        }
+        return this._avatar;        
+    }
+    get createdBy ()
+    {
+        return this._app.storeState.chatUsers?.[ this._createdBy ] ?? null;        
+    }
+    get members ()
+    {
+        return this._members
+            .map( userId => (this._app.storeState.chatUsers?.[ String(userId) ] ?? null) )
+            .filter(userFields => !!userFields)
+            .map( userFields => new ChatUser(userFields) );
+    }  
+    get collocutor ()
+    {
+        const collocutorId = this._members.filter( user => user == this._userId )[0];
+
+        if (collocutorId)
+        {
+            const collocutor = this._app.storeState.chatUsers?.[ String(collocutorId) ];
+
+            return collocutor ? new ChatUser( collocutor ) : null;
+        }
+        return null;
+    }  
     get messages ()
     {
-        return chatMessages;
+        return chatsApi.getNewMsgCnt(this._id)
+            .then(unreadCnt => 
+            {
+                return (async () =>
+                {
+                    let messages : ChatMessage[] = [];                    
+                    do
+                    {
+                        messages = messages.concat( await this._messenger.getHistory() );
+                    } 
+                    while (messages.length < unreadCnt);
+
+                    return messages;
+                })();
+            })
+            .catch(error => apiErrorHandler(error));
     }
-    onClose ()
+    get lastMessage ()
     {
-        // TODO когда закрываем чат закроем сокет 
+        return this._app.storeState.chats?.[ String(this._id) ].lastMessage;
+    }
+    sendMessage (content : string, type : MessageType = 'message')
+    {
+        return this._messenger.send(content, type);
+    }
+    onDelete ()
+    {
+        this._messenger.close();
+    }
+    protected _isUserChat ()
+    {
+        return ChatType.user == this._type;
+    }
+    protected _processChatType ()
+    {
+        // TODO define by ' vs ' substr in title and 2 == this._members.length 
+        this._type = ChatType.user;
+    }
+    static getUserChatName(user : ChatUser, collocutor : ChatUser)
+    {
+        return `${user?.nickname} vs ${collocutor.nickname}`;
     }
 }

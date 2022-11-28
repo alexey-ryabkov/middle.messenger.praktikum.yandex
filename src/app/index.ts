c640c2fed91cdebcb9e6b18c2733d38edf42155f
alexey-ryabkov <xelaris@ya.ru>
2022-11-28 12:34:46 +0300

    completion of app to work with store and models

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,13 @@
 
-import {App, AppContainer, Nullable} from '@core/types';
+import {App, AppContainer} from '@core/types';
 import Page, {PageAccess} from '@core/page';
 import Router from '@core/router';
-import ChatList from '@models/chat_list';
-import CurrentUser from '@models/user';
-import Store from '@core/state/store';
-import userApi from '@api/user';
+import Store, { StoreEvents } from '@core/state/store';
+import {AppStoreScheme} from '@models/types';
+import ChatsList from '@models/chats_list'; 
+import CurrentUser from '@models/current_user';
 import MainContainer from '@app-modules/main';
-import ChatUser from '@models/chat_user';
 import Spinner from '@lib-components/spinner';
-import { ProfileData } from '@models/types';
 
 export default class SurChat implements App
 {
@@ -25,60 +23,28 @@ export default class SurChat implements App
     protected _container : AppContainer; 
     protected _router : Router;
     protected _store : Store;
-    protected _chatsList : ChatList;
-    protected _user : Nullable< CurrentUser > = null;
-    // TODO isUserAuthorized 
-    protected _isUserDefined = false;
+    protected _chatsList : ChatsList;
+    protected _user : CurrentUser;
 
     private static _instance : SurChat;
 
     private constructor()
     {
-        // FIXME 
-        // console.log(Math.random());
-        
-        this._container = new MainContainer( SurChat._ROOT_NODE, new Spinner({ size: 'large' }) ).mount(); 
         this.title = SurChat._INITIALIZE_MSG;
-
+        this._container = new MainContainer( SurChat._ROOT_NODE, new Spinner({ size: 'large' }) ).mount(); 
+        
         this._router = new Router();
-        this._store = new Store();
 
-        // TODO привести к единообразию - где-то currentUser ге-то profile         
-        this._store.oneTime('updated:current_user', currentUser => 
-        {
-            console.log('oneTime updated:current_user', currentUser);
-            return this._isUserDefined = !!currentUser
-        });
+        const initState : AppStoreScheme = {
+            currentUser: null,
+            chats: {}, 
+            openedChat: null,
+            chatUsers: {},
+        };
+        this._store = new Store(initState, `${SurChat.NAME} store`);
 
-        this._user = new CurrentUser();
-        this._chatsList = new ChatList();    
-
-        // window.addEventListener('popstate', ((event : PopStateEvent) => 
-        // {
-        //     console.log('onpopstate in app', event.state, event);
-
-        //     const url = (event.currentTarget as Window).location.pathname;
-
-        //     this._redirectIfNoAccess(url);
-        // })
-        // .bind(this));
-
-        // TODO неплохо бы убирать в подметоды 
-        SurChat._ROOT_NODE.addEventListener('click', event =>
-        {
-            const element = event.target as Element;
-
-            if ('A' == element.tagName) 
-            {
-                event.preventDefault();
-
-                const url = element.getAttribute('href');
-                if (url)
-                {
-                    this.go2url(url);
-                }        
-            }
-        }); 
+        this._user = new CurrentUser(this);
+        this._chatsList = new ChatsList(this); 
     }
     static get instance ()
     {
@@ -97,63 +63,47 @@ export default class SurChat implements App
     {
         return this._container;
     }
-    get chatsList ()
+    get store ()
     {
-        return this._chatsList;
+        return this._store;
+    }
+    get storeState ()
+    {
+        return this._store.state as AppStoreScheme;
     }
     get user ()
     {
         return this._user;
     }
-    get store ()
+    get isUserAuthorized ()
     {
-        return this._store;
+        return !!this.user.data;
     }
-    // get router ()
-    // {
-    //     return this._router;
-    // }
-    // logoutUser ()
-    // {
-    //     this._user = null;
-    //     // TODO тогда chatList, котором передается пользователь также обнуляется? 
-
-
-    //     this.go2page( SurChat.AUTH_PAGE_NAME );
-    // }
-    // TODO убрать весь сахар, который не используется по факту, итак класс объемный по функционалу 
-    get page ()
+    get chatsList ()
     {
-        return this._router.currentRoute;
+        return this._chatsList;
     }
+    // TODO убрать весь сахар: .page .go2page ? 
     set pages (pages : Page[])
     {
         pages.forEach( page => this._router.use(page) );
     }
+    get page ()
+    {
+        return this._router.currentRoute;
+    }    
     init ()
     {
+        this._prepareLinks4routing();
+        this._prepareAuthRedirects();
+        
         if (!this._redirectIfNoAccess(this._router.curPathname) && !this._router.start())
         {
             this._router.go( Page.url( SurChat.FALLBACK_PAGE_NAME ));
         }
-
-        this._store.on('updated:current_user', currentUser => 
-        {
-            console.log('on updated:current_user', currentUser);
-            if (this._isUserDefined && null === currentUser)
-            {
-                this._router.go( Page.url( SurChat.AUTH_PAGE_NAME ));
-            }
-            else if (!this._isUserDefined && currentUser)
-            {
-                this._router.go( Page.url( SurChat.CHAT_PAGE_NAME ));
-            }
-            this._isUserDefined = !!currentUser;
-        });
     }
     go2url (url : string)
     {      
-        // TODO по идее нужно показывать 403 ошибку...  
         if (!this._redirectIfNoAccess(url) && !this._router.go(url))
         {
             this._router.go( Page.url( SurChat.FALLBACK_PAGE_NAME ));
@@ -163,8 +113,6 @@ export default class SurChat implements App
     {
         this.go2url( Page.url(name) );
     }
-
-    // TODO checkAccess methid for Page, need public isUserDefined 
     protected _redirectIfNoAccess (url : string)
     {   
         const page = this._router.getRoute(url) as Page | null;
@@ -173,15 +121,50 @@ export default class SurChat implements App
         {
             return false;
         } 
-        if (!this._isUserDefined && page.access == PageAccess.authorized)
+        if (!this.isUserAuthorized && page.access == PageAccess.authorized)
         {
             return this._router.go( Page.url( SurChat.AUTH_PAGE_NAME ));
         }
-        else if (this._isUserDefined && page.access == PageAccess.nonAuthorized)
+        else if (this.isUserAuthorized && page.access == PageAccess.nonAuthorized)
         {
             return this._router.go( Page.url( SurChat.CHAT_PAGE_NAME ));
         }
         return false;
+    }
+    protected _prepareLinks4routing ()
+    {
+        SurChat._ROOT_NODE.addEventListener('click', event =>
+        {
+            const element = event.target as Element;
+
+            if ('A' == element.tagName) 
+            {
+                event.preventDefault();
+
+                const url = element.getAttribute('href');
+                if (url)
+                {
+                    this.go2url(url);
+                }        
+            }
+        });
+    }
+    protected _prepareAuthRedirects ()
+    {
+        // Store.getEventName4path('currentUser')
+        // 'updated:currentUser'
+        this._store.on(StoreEvents.updated, user => 
+        {
+            console.log('that`s it');
+            if (this.isUserAuthorized && null === user)
+            {
+                this._router.go( Page.url( SurChat.AUTH_PAGE_NAME ));
+            }
+            else if (!this.isUserAuthorized && user)
+            {
+                this._router.go( Page.url( SurChat.CHAT_PAGE_NAME ));
+            }
+        });
     }
 }
 window.app = SurChat.instance;

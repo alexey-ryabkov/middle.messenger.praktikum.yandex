582202f2ab7376e9ca6c31201bbe15c8895c2710
alexey-ryabkov <xelaris@ya.ru>
2022-12-10 02:07:07 +0300

    setup webpack bundling

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV;
+const isProdMode = process.env.NODE_ENV == 'production';
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+module.exports = {
+    mode: mode || 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: `bundle${!isProdMode ? '-[hash]' : ''}.js`,
+    },
+    resolve: {
+        extensions: ['.ts'],
+        alias: {
+            'handlebars' : 'handlebars/dist/handlebars.js'
+        },
+        plugins: [
+            new TsconfigPathsPlugin(),
+        ],
+    },
+    devtool: !isProdMode ? 'source-map' : false,
+    devServer: {
+        port: 1234,
+        hot: !isProdMode,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin(
+        {
+            template: './src/index.html',
+            minify: {
+                removeComments: isProdMode,
+                collapseWhitespace: isProdMode
+            },
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin(
+        {
+            filename: `bundle${!isProdMode ? '-[hash]' : ''}.css`,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [ 'ts-loader' ],
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    stylesHandler,
+                    'css-loader',   
+                    'sass-loader',                                    
+                ],
+                exclude: '/static/layout/',
+            },
+            {
+                test: /\.(?:woff(2)?|eot|ttf|otf|svg|ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset',
+                exclude: '/static/layout/',
+            },
+        ]
+    }
+}; 

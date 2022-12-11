7e126cc32fb48561b8d2ef3dfe87c1355c718674
alexey-ryabkov <xelaris@ya.ru>
2022-12-11 06:40:14 +0300

    some improvements for tests

--- src/test/mock.ts
+++ src/test/mock.ts
@@ -1,8 +1,11 @@
 import {JSDOM} from 'jsdom';
 
-const {window} = new JSDOM('<div id="mock-app"></div>', 
+const {window} = new JSDOM('<div class="_mockApp"></div>', 
 { 
     url: 'http://localhost:3000' 
 });
 global.window = <unknown>window as Window & typeof globalThis;
 global.document = window.document;
+global.FormData = window.FormData;
+global.XMLHttpRequest = window.XMLHttpRequest;
+global.HTMLElement = window.HTMLElement;

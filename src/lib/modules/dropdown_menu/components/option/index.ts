a649b19a38dfd98526977a533413b35dba8111dc
alexey-ryabkov <xelaris@ya.ru>
2022-12-12 07:10:40 +0300

    close menu after option choosed

--- src/lib/modules/dropdown_menu/components/option/index.ts
+++ src/lib/modules/dropdown_menu/components/option/index.ts
@@ -1,6 +1,7 @@
 import {Handler} from '@core/types';
 import Templator from '@core/templator';
 import {BlockEvents, BlockProps} from '@core/block';
+import DropdownMenu from '@lib-modules/dropdown_menu';
 import ComponentBlock from '@core/block/component';
 import {BemParams} from '@core/block/bem';
 import Icon, {IconVar} from '@lib-components/icon';
@@ -10,20 +11,21 @@ export type DropdownMenuOptionProps = BlockProps & {
     title : string,
     icon? : IconVar,
     url? : string,
-    action? : Handler,
+    action? : (menu : DropdownMenu | null) => void,
 };
-
 export default class DropdownMenuOption extends ComponentBlock 
 {
     constructor (props : DropdownMenuOptionProps)
     {
         const {title, icon: iconVariant, url = '#', action} = props;
         
-        const events : BlockEvents = action ? ['click', event => 
-        {
-            event.preventDefault();
-            action();
-        }] : [];
+        const events : BlockEvents = action 
+            ? ['click', event => 
+            {
+                event.preventDefault();
+                action( DropdownMenuOption._getDropdownMenuComponent4optionElement( event.currentTarget as Element ));
+            }] 
+            : [];
 
         let icon;
         if (iconVariant)
@@ -35,6 +37,12 @@ export default class DropdownMenuOption extends ComponentBlock
             icon.bemMix(['dropdownMenu', 'menuOptIcon']);    
         }
         super( {title, icon, url}, events, {node: 'li'} );
+    }
+    protected static _getDropdownMenuComponent4optionElement (optionElement : Element)
+    {
+        const menuElement = optionElement.closest('.dropdownMenu');
+
+        return menuElement ? DropdownMenu.getMenuComponent( menuElement ) : null;
     }
     protected _prepareBemParams ()
     {
